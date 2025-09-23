08676d3dfcbe9cb420deaa7f6068c9bb96eff3c9
Leonardo Javier Russo <info@lrusso.com>
2025-09-22 21:35:02 -0300

    Initial commit

---
+++ Taipei/worker.js
@@ -0,0 +1,25 @@
+const filesToCache = ["index.html"]
+
+const staticCacheName = "taipei-v1"
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      return cache.addAll(filesToCache)
+    })
+  )
+})
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          return response
+        }
+        return fetch(event.request)
+      })
+      .catch((error) => {})
+  )
+})
