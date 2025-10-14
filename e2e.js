0575d4e20f6fda20f9c8fefd199d0e3c449c898c
Leonardo Javier Russo <info@lrusso.com>
2025-10-14 17:14:27 -0300

    Improving e2e tests

--- e2e.js
+++ e2e.js
@@ -1,10 +1,18 @@
 import { execSync } from "child_process"
 import process from "process"
 
-const browseToWebsite = (userLanguage, userAgent, url) => {
+const browseToWebsite = (userLanguage, userAgent, url, selector) => {
   try {
     const result = execSync(
-      'java -jar e2e.jar "' + userLanguage + '" "' + userAgent + '" "' + url + '"'
+      'java -jar e2e.jar "' +
+        userLanguage +
+        '" "' +
+        userAgent +
+        '" "' +
+        url +
+        '" "' +
+        selector +
+        '"'
     )
       .toString()
       .trim()
@@ -16,49 +24,37 @@ const browseToWebsite = (userLanguage, userAgent, url) => {
   return ""
 }
 
-const findWord = (language, userAgent, url, word) => {
-  try {
-    const localWebsite = JSON.parse(
-      browseToWebsite(language, userAgent, url)
-    ).innerText
-    if (localWebsite.includes(word)) {
-      return true
-    }
-  } catch (err) {
-    //
-  }
-
-  // eslint-disable-next-line no-console
-  console.log(language + " - " + url + ' is not showing the "' + word + '" word.')
-  return false
-}
-
-const checkWebsite = (url) => {
-  for (const lang in languages) {
-    try {
-      const toFind = languages[lang]
-      const result = findWord(lang, userAgent, url, toFind)
-      if (!result) {
-        errorsFound = true
-      }
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.log(err)
-      errorsFound = true
-    }
-  }
-}
-
+const url = "index.html"
+const langEN = "en-US"
+const langES = "es-AR"
 const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/117.0"
-const languages = {
-  "es-AR": "desarrollos",
-  "en-US": "projects",
-}
+const linkPlayStore = "#home > div:nth-child(12) > div:nth-child(3) > div > div > a"
 
 let errorsFound = false
 
-checkWebsite("https://www.lrusso.com")
-checkWebsite("index.html")
+// TEST 1 - CHECKING WORDS IN ENGLISH
+const englishTest = browseToWebsite(langEN, userAgent, url)
+if (!englishTest.includes("projects")) {
+  errorsFound = true
+  // eslint-disable-next-line no-console
+  console.log(langEN + " - " + url + ' is not showing the \"projects\" word.')
+}
+
+// TEST 2 - CHECKING WORDS IN SPANISH
+const spanishTest = browseToWebsite(langES, userAgent, url)
+if (!spanishTest.includes("desarrollos")) {
+  errorsFound = true
+  // eslint-disable-next-line no-console
+  console.log(langES + " - " + url + ' is not showing the \"desarrollos\" word.')
+}
+
+// TEST 3 - CLICKING ON THE PLAY STORE ICON
+const playStoreTest = browseToWebsite(langEN, userAgent, url, linkPlayStore)
+if (!playStoreTest.includes("3D Object Viewer")) {
+  errorsFound = true
+  // eslint-disable-next-line no-console
+  console.log("The Play Store is not working.")
+}
 
 if (errorsFound) {
   process.exit(1)
