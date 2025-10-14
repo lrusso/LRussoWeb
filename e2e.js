cd35775943a93a822c52b076797e3602e3afced7
Leonardo Javier Russo <info@lrusso.com>
2025-10-14 17:26:14 -0300

    Adding end to end tests

--- e2e.js
+++ e2e.js
@@ -16,7 +16,7 @@ const browseToWebsite = (userLanguage, userAgent, url, selector) => {
     )
       .toString()
       .trim()
-    return result
+    return JSON.parse(result).innerText
   } catch (err) {
     //
   }
@@ -28,6 +28,7 @@ const url = "index.html"
 const langEN = "en-US"
 const langES = "es-AR"
 const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/117.0"
+const linkLinkedIn = "#home > div:nth-child(12) > div:nth-child(2) > div > div > a"
 const linkPlayStore = "#home > div:nth-child(12) > div:nth-child(3) > div > div > a"
 const linkGitHub = "#home > div:nth-child(12) > div:nth-child(4) > div > div > a"
 
@@ -38,7 +39,7 @@ const englishTest = browseToWebsite(langEN, userAgent, url)
 if (!englishTest.includes("projects")) {
   errorsFound = true
   // eslint-disable-next-line no-console
-  console.log(langEN + " - " + url + ' is not showing the \"projects\" word.')
+  console.log(langEN + ' - The "projects" word was not found.')
 }
 
 // TEST 2 - CHECKING WORDS IN SPANISH
@@ -46,10 +47,18 @@ const spanishTest = browseToWebsite(langES, userAgent, url)
 if (!spanishTest.includes("desarrollos")) {
   errorsFound = true
   // eslint-disable-next-line no-console
-  console.log(langES + " - " + url + ' is not showing the \"desarrollos\" word.')
+  console.log(langES + ' - The "desarrollos" word was not found.')
 }
 
-// TEST 3 - CLICKING ON THE PLAY STORE ICON
+// TEST 3 - CLICKING ON THE LINKEDIN ICON
+const linkedInTest = browseToWebsite(langEN, userAgent, url, linkLinkedIn)
+if (!linkedInTest.includes("Mobile, Web and AI developer")) {
+  errorsFound = true
+  // eslint-disable-next-line no-console
+  console.log("The LinkedIn link is not working.")
+}
+
+// TEST 4 - CLICKING ON THE PLAY STORE ICON
 const playStoreTest = browseToWebsite(langEN, userAgent, url, linkPlayStore)
 if (!playStoreTest.includes("3D Object Viewer")) {
   errorsFound = true
@@ -57,7 +66,7 @@ if (!playStoreTest.includes("3D Object Viewer")) {
   console.log("The Play Store link is not working.")
 }
 
-// TEST 4 - CLICKING ON THE GITHUB ICON
+// TEST 5 - CLICKING ON THE GITHUB ICON
 const githubTest = browseToWebsite(langEN, userAgent, url, linkGitHub)
 if (!githubTest.includes("Popular repositories")) {
   errorsFound = true
