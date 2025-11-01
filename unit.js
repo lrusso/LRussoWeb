4ea96b308cc374590124bf932473ba3b6a5a8867
Leonardo Javier Russo <info@lrusso.com>
2025-11-01 19:04:38 -0300

    Normalizing code

--- unit.js
+++ unit.js
@@ -860,7 +860,7 @@ describe("ResumeChecker/index.html", function () {
   })
 
   it("Should not have changed", function () {
-    const expectedHash = "3e89e05b"
+    const expectedHash = "46f12e43"
     let currentHash = ""
 
     try {
