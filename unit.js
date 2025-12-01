5a02a42f3dfb925f4514ec8db04876c85ae04240
Leonardo Javier Russo <info@lrusso.com>
2025-11-30 23:58:08 -0300

    Updating Emulatrix

--- unit.js
+++ unit.js
@@ -565,7 +565,7 @@ describe("Emulatrix/index.html", function () {
   })
 
   it("Should not have changed", function () {
-    const expectedHash = "324b6839"
+    const expectedHash = "38c3e76c"
     let currentHash = ""
 
     try {
