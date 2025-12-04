35263e3b734bdc7952d1a386eb8a4d9a40b84cd9
Leonardo Javier Russo <info@lrusso.com>
2025-12-04 20:14:13 -0300

    Improving compatibility

--- unit.js
+++ unit.js
@@ -1039,7 +1039,7 @@ describe("Taipei/index.html", function () {
   })
 
   it("Should not have changed", function () {
-    const expectedHash = "2ef05354"
+    const expectedHash = "564a31b2"
     let currentHash = ""
 
     try {
@@ -1060,6 +1060,10 @@ describe("Taipei/index.html", function () {
   it("Should have equal i18n key counts across languages", function () {
     const APP_STRINGS = getVariable("Taipei/index.html", "APP_STRINGS")
       .replace(/,\s*([}\]])/g, "$1")
+      // convert hex escapes
+      .replace(/\\x([0-9A-Fa-f]{2})/g, function (_, hex) {
+        return String.fromCharCode(parseInt(hex, 16))
+      })
       .replace(/([{\s,])([a-zA-Z0-9_]+)\s*:(?=(?:[^"]*"[^"]*")*[^"]*$)/g, '$1"$2":')
 
     const sameAmount = langsCheckAmountKeys(APP_STRINGS)
@@ -1070,6 +1074,10 @@ describe("Taipei/index.html", function () {
   it("Should have matching i18n keys across languages", function () {
     const APP_STRINGS = getVariable("Taipei/index.html", "APP_STRINGS")
       .replace(/,\s*([}\]])/g, "$1")
+      // convert hex escapes
+      .replace(/\\x([0-9A-Fa-f]{2})/g, function (_, hex) {
+        return String.fromCharCode(parseInt(hex, 16))
+      })
       .replace(/([{\s,])([a-zA-Z0-9_]+)\s*:(?=(?:[^"]*"[^"]*")*[^"]*$)/g, '$1"$2":')
 
     const allLanguagesHaveSameKeys = langsCheckSameKeys(APP_STRINGS)
@@ -1080,6 +1088,10 @@ describe("Taipei/index.html", function () {
   it("Should have non-empty i18n keys across languages", function () {
     const APP_STRINGS = getVariable("Taipei/index.html", "APP_STRINGS")
       .replace(/,\s*([}\]])/g, "$1")
+      // convert hex escapes
+      .replace(/\\x([0-9A-Fa-f]{2})/g, function (_, hex) {
+        return String.fromCharCode(parseInt(hex, 16))
+      })
       .replace(/([{\s,])([a-zA-Z0-9_]+)\s*:(?=(?:[^"]*"[^"]*")*[^"]*$)/g, '$1"$2":')
 
     const allLanguagesHaveNoEmptyKeys = langsCheckEmptyKeys(APP_STRINGS)
