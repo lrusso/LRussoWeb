47144a88ac0a4c931f1f7ef581fae2cf838fd8a4
Leonardo Javier Russo <info@lrusso.com>
2026-02-22 02:14:32 -0300

    Update Intranet

--- unit.js
+++ unit.js
@@ -577,7 +577,7 @@ describe("Intranet/index.html", function () {
   })
 
   it("Should not have changed", function () {
-    const expectedHash = "5f71631c"
+    const expectedHash = "6594e22a"
     let currentHash = ""
 
     try {
