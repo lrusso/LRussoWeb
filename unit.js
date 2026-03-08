19040eebb47673e152c2060f488c22bc33b8d3bf
Leonardo Javier Russo <info@lrusso.com>
2026-03-07 22:21:03 -0300

    Fix text render

--- unit.js
+++ unit.js
@@ -1444,7 +1444,7 @@ describe("Tarot/index.html", function () {
   })
 
   it("Should not have changed", function () {
-    const expectedHash = "7d9a92d7"
+    const expectedHash = "1dbc4803"
     let currentHash = ""
 
     try {
